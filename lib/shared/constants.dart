2ba9c7f2311139bd1a51dc9db14f22897de9212d
MhdZiadHirati <zead.heraty@gmail.com>
2023-04-16 14:55:54 +0300

    comment

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+//you can put your own api key here
 String apiKey = '6e5ec4065caeae35bb484e47f7ad0ea6';
-String darkModeBackGroundColorhex = '333739';
-void navigateTo(context,widget){
-  Navigator.push(context, MaterialPageRoute(builder: (context)=>widget));
 
+String darkModeBackGroundColorhex = '333739';
+
+void navigateTo(context, widget) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
 }
